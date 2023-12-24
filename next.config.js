d0724a0c5c6c405b2fcd3974e12226fc3aea4ce3
Debuisson Julian <80641015+JulianDebuisson@users.noreply.github.com>
2023-12-24 17:54:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  // ...autres configurations Next.js
+
+  // Ajoutez cette configuration pour l'export statique
+  // Vous pouvez personnaliser cette configuration en fonction de vos besoins
+  experimental: {
+    output: 'export', // Cette ligne indique à Next.js d'utiliser l'export statique
+  },
+};
